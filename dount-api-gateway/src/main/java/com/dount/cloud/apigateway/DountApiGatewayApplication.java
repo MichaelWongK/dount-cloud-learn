5925201de4c07f85464d1da261b7a98b7df81b24
MichaelWongK <michael.won007@gmail.com>
2020-02-11 18:37:00 +0800

    feat: api-gatewayyml文件配置路由

---
+++ dount-api-gateway/src/main/java/com/dount/cloud/apigateway/DountApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dount.cloud.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DountApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DountApiGatewayApplication.class, args);
+	}
+
+}
---
+++ dount-api-gateway/src/main/java/com/dount/cloud/apigateway/config/GatewayConfig.java
@@ -0,0 +1,4 @@
+package com.dount.cloud.apigateway.config;
+
+public class GatewayConfig {
+}
